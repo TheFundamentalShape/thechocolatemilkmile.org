21bdd990cc6c5b61b2691dad982e04a3841fa62c
Luigi Battaglioli <him@theluigi.com>
2021-07-30 00:14:50 -0400

    added confirmation number to registrants table

--- resources/js/components/RegistrantsList.vue
+++ resources/js/components/RegistrantsList.vue
@@ -13,7 +13,7 @@
                         Name
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        Miletime
+                        Confirmation Number
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Status
@@ -41,7 +41,7 @@
                         </div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-900">{{ registration.registrant.mile_time }}</div>
+                        <div class="text-sm text-gray-900">{{ registration.confirmation_number }}</div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
                         <span v-show="registration.checked_in_at === null" class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
